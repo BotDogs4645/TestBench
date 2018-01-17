92a91eaa6f11ddad52bb7881b5a85dbde98e7db4
Alexa Lagunas <amlagunas70@gmail.com>
2018-01-17 16:24:31 -0600

    commented
    documented code

--- src/org/usfirst/frc/team4645/robot/subsystems/LimitSwitch.java
+++ src/org/usfirst/frc/team4645/robot/subsystems/LimitSwitch.java
@@ -15,10 +15,23 @@ public class LimitSwitch extends Subsystem
 {
 	//declared limit switch and counter variables
 	static DigitalInput limitSwitch;
-	static Counter counter = new Counter(limitSwitch);
+	static Counter counter1 = new Counter(limitSwitch);
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
-
+	
+	
+	//tells if switch is set (1)
+	public static boolean isSwitchSet()
+	{
+		return counter1.get()>0;
+	}
+	
+	//resets counter
+    public void initializeCounter() {
+        counter1.reset();
+    }	
+	
+	
 	public void initDefaultCommand() 
 	{
 		// Set the default command for a subsystem here.
@@ -26,9 +39,6 @@ public class LimitSwitch extends Subsystem
 		
 	}
 	
-	public static boolean isSwitchSet()
-	{
-		return counter.get()>0;
-	}
+
 	
 }
