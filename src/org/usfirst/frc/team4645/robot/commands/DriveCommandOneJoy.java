ba81692b3d0a3ba5f533b8cd5898854cb9cc2b60
Alexa Lagunas <amlagunas70@gmail.com>
2018-01-25 18:32:24 -0600

    working tank drive!
    tested program that slaves motors

--- src/org/usfirst/frc/team4645/robot/commands/DriveCommandOneJoy.java
+++ src/org/usfirst/frc/team4645/robot/commands/DriveCommandOneJoy.java
@@ -1,8 +1,5 @@
 package org.usfirst.frc.team4645.robot.commands;
-
 import org.usfirst.frc.team4645.robot.Robot;
-import org.usfirst.frc.team4645.robot.subsystems.TankDriveOneJoy;
-
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -12,7 +9,8 @@ public class DriveCommandOneJoy extends Command
 {
 	
 
-    public DriveCommandOneJoy() {
+    public DriveCommandOneJoy() 
+    {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	
@@ -20,31 +18,34 @@ public class DriveCommandOneJoy extends Command
     }
 
     // Called just before this Command runs the first time
-    protected void initialize() {
+    protected void initialize() 
+    {
+    		Robot.tankDriveSubsystem.init();
     }
 
     // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    		
-    	
-    		
-    		TankDriveOneJoy.driveWithJoystick();
+    protected void execute() 
+    {	
+    		Robot.tankDriveSubsystem.driveWithJoystick();
     }
 
     // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
+    protected boolean isFinished() 
+    {
         return false;
     }
 
     // Called once after isFinished returns true
-    protected void end() {
+    protected void end() 
+    {
     	
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
-    protected void interrupted() {
-    	  TankDriveOneJoy.stop();
+    protected void interrupted() 
+    {
+    	  Robot.tankDriveSubsystem.stop();
     		
     }
 }
--- src/org/usfirst/frc/team4645/robot/subsystems/TankDriveOneJoy.java
+++ src/org/usfirst/frc/team4645/robot/subsystems/TankDriveOneJoy.java
@@ -2,12 +2,9 @@ package org.usfirst.frc.team4645.robot.subsystems;
 import org.usfirst.frc.team4645.robot.OI;
 import org.usfirst.frc.team4645.robot.RobotMap;
 import org.usfirst.frc.team4645.robot.commands.DriveCommandOneJoy;
-
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
-//import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
@@ -19,17 +16,15 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class TankDriveOneJoy extends Subsystem 
 {
 	
-	private static WPI_TalonSRX motorL1 = new WPI_TalonSRX(RobotMap.left1);
-	private static WPI_TalonSRX motorL2 = new WPI_TalonSRX(RobotMap.left2);
-	private static WPI_TalonSRX motorL3 = new WPI_TalonSRX(RobotMap.left3);
-	private static SpeedControllerGroup leftSide = new SpeedControllerGroup(motorL1, motorL2, motorL3);
+	public WPI_TalonSRX motorL1 = new WPI_TalonSRX(RobotMap.left1);
+	public WPI_TalonSRX motorL2 = new WPI_TalonSRX(RobotMap.left2);
+	public  WPI_TalonSRX motorL3 = new WPI_TalonSRX(RobotMap.left3);
 	
-	private static WPI_TalonSRX motorR1 = new WPI_TalonSRX(RobotMap.right1);
-	private static WPI_TalonSRX motorR2 = new WPI_TalonSRX(RobotMap.right2);
-	private static WPI_TalonSRX motorR3 = new WPI_TalonSRX(RobotMap.right3);
-	private static SpeedControllerGroup rightSide = new SpeedControllerGroup(motorR1, motorR2, motorR3);
+	public WPI_TalonSRX motorR1 = new WPI_TalonSRX(RobotMap.right1);
+	public WPI_TalonSRX motorR2 = new WPI_TalonSRX(RobotMap.right2);
+	public WPI_TalonSRX motorR3 = new WPI_TalonSRX(RobotMap.right3);
 	
-	
+	DifferentialDrive robotDrive = new DifferentialDrive(motorL1, motorR1);
 
     public void initDefaultCommand() 
     {
@@ -37,8 +32,52 @@ public class TankDriveOneJoy extends Subsystem
     		setDefaultCommand(new DriveCommandOneJoy());
     }
     
-    public static void driveWithJoystick()
+    public void init()
     {
+    	//motors connected to "middle" gear are slaved on both sides
+		motorL2.follow(motorL1);
+		motorL3.follow(motorL1);
+		motorL2.setInverted(true);
+		motorL3.setInverted(true);
+		
+		
+		motorR2.follow(motorR1);
+		motorR3.follow(motorR1);
+		motorR2.setInverted(true);
+		motorR3.setInverted(true);
+		
+    	
+    }
+    
+    public void driveWithJoystick()
+    {
+		double forward = OI.joystick1.getY();
+		
+		/* sign this so right is positive. */
+		double turn = OI.joystick1.getZ();
+		
+		
+		
+		/* deadband */
+		if (Math.abs(forward) < 0.10) {
+			/* within 10% joystick, make it zero */
+			forward = 0;
+		}
+		if (Math.abs(turn) < 0.10) {
+			/* within 10% joystick, make it zero */
+			turn = 0;
+		}
+		
+
+		SmartDashboard.putNumber("JoyY:",  forward);
+		SmartDashboard.putNumber("Turn", turn);
+
+//what does forward do? what does turn do?
+		robotDrive.arcadeDrive(forward, turn);		
+	}
+    	
+    	
+    /*
     	//sets both motor speeds with joystick Y-values, with left side going clockwise, right side going counter-clockwise
     		rightSide.set(-OI.joystick1.getY());  		
     		leftSide.set( OI.joystick1.getY());
@@ -52,19 +91,15 @@ public class TankDriveOneJoy extends Subsystem
 
     	  
     }
-    
-    public static void stop()
+    */
+    public void stop()
     {
     	
 		motorL1.set(0);
-		motorL2.set(0);		
-		motorL3.set(0);
-		
+	
 		motorR1.set(0);
-		motorR2.set(0);
-		motorR3.set(0);
-    	
-   	
-    }
+
+	}
+    
 }
 
